a908572a560789ae17a5e665d8e50f22fde30b79
Bambe Chen <ayuha406645@gmail.com>
2018-06-15 18:26:09 +0800

    render api data

--- stage2/src/app/home/home.component.ts
+++ stage2/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from "../shared.service";
+import { WebapiService } from "../webapi.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  article_datas: Array<any>;
+  total: Number;
+
+  constructor(
+    private _shared: SharedService,
+    private _webapi: WebapiService
+  ) { }
 
   ngOnInit() {
+    if (!this._shared.article_data) {
+      this._webapi.getArticleData().subscribe(data => {
+        this._shared.article_data = this.article_datas = data['result'].records;
+        this._shared.article_data_total = this.total = data['result'].total;
+      });
+    } else {
+      this.article_datas = this._shared.article_data;
+      this.total = this._shared.article_data_total;
+    }
   }
 
 }
